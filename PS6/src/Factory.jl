31d1f4c79317de7188fd8ef8adc389a94b880686
ks929 <113550992+ks929@users.noreply.github.com>
2023-04-11 14:39:12 -0400

    implementing build

--- PS6/src/Factory.jl
+++ PS6/src/Factory.jl
@@ -48,6 +48,11 @@ function _build_reaction_id_array(data::Dict{String,Any})::Array{String,1}
 
     # TODO: fill the reaction_id_array with the reaction id's from the data dictionary
     # ....
+    reactions = data["reactions"];
+    for reaction ∈ reactions
+        id_value = reaction["id"];
+        push!(reaction_id_array, id_value);
+    end 
 
     # return -
     return reaction_id_array;
@@ -65,6 +70,13 @@ function _build_bounds_array(data::Dict{String,Any})::Array{Float64,2}
 
     # TODO: fill in the entries of the bounds array, first col is the lower bound, second col is the upper bound
     # ...
+    for i ∈ 1:number_of_reactions
+        reaction = list_of_reactions[i];
+        L = reaction["lower_bound"];
+        U = reaction["upper_bound"];
+        bounds_array[i,1] = L
+        bounds_array[i,2] = U
+    end
 
     # return -
     return bounds_array
