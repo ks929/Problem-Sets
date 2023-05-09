7748b36f39c1862aefcab504289d60864e380562
ks929 <113550992+ks929@users.noreply.github.com>
2023-05-09 18:02:42 -0400

    fixed PS5!

--- PS5/src/Compute.jl
+++ PS5/src/Compute.jl
@@ -7,36 +7,36 @@ with the first column being all 1's and the second column being a 1/S value.
 function build_data_matrix(data::DataFrame)::Array{Float64,2}
 
     # initialize -
-    number_of_repeats = 3;
+    iterate = 3;
     
     # get the size of the data -
     (R,C) = size(data);
 
     # build an 1/s array -
-    S_array = Array{Float64,1}();
+    matrixarray = Array{Float64,1}();
     for i ∈ 1:R
-        S_value = data[i,:S];
-        for j ∈ 1:number_of_repeats
-            push!(S_array,(1/S_value));
+        value = data[i,:S];
+        for j ∈ 1:iterate
+            push!(matrixarray,(1/(value)));
         end
     end
 
     # construct the data matrix -
-    X = Array{Float64,2}(undef, number_of_repeats*R, 2);
-    for i ∈ 1:number_of_repeats:(number_of_repeats*R)
-        for j ∈ i:(i+number_of_repeats-1)
+    Data_matrix = Array{Float64,2}(undef, iterate*R, 2);
+    for i ∈ 1:iterate:(iterate*R)
+        for j ∈ i:(i+iterate-1)
             
             # get the S level -
-            S_value = S_array[j];
+            value = matrixarray[j];
             
             # fill in the columns of the data matrix -
-            X[j,1] = 1.0;
-            X[j,2] = S_value
+            Data_matrix[j,1] = 1.0;
+            Data_matrix[j,2] = value
         end
     end
 
     # return -
-    return X
+    return Data_matrix
 end
 
 """
@@ -48,22 +48,22 @@ The output vector will be a experiments*repeats x 1 vector holding the 1/v value
 function build_output_vector(data::DataFrame)::Array{Float64,1}
 
     # initialize -
-    number_of_repeats = 3;
+    iterate = 3;
 
     # get the size of the data -
     (R,C) = size(data);
 
     # build the output array -
-    Y_array = Array{Float64,1}();
+    vectorarray = Array{Float64,1}();
     for i ∈ 1:R
-        for j ∈ 1:number_of_repeats
-            y_value = data[i,j+1];
-            push!(Y_array, (1/y_value));
+        for j ∈ 1:iterate
+            value2 = data[i,j+1];
+            push!(vectorarray, (1/value2));
         end
     end
 
     # return -
-    return Y_array;
+    return vectorarray;
 end
 
 """
